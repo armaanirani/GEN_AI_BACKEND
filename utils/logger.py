97ae7398edf260f54f403778d740fb5dc18de516
Armaan Irani <armaanirani@gmail.com>
2025-05-11 23:00:18 +0530

    fixed typo

--- utils/logger.py
+++ utils/logger.py
@@ -10,13 +10,13 @@ os.makedirs(LOG_DIR, exist_ok=True)
 logger = logging.getLogger("llm_api")
 logger.setLevel(logging.INFO)
 
-file_handler = RotatingFileHandler(LOG_PATH, maxBytes=10 * 1024 * 1024, backupCount=5)
+file_handler = RotatingFileHandler(LOG_PATH, maxBytes=5_000_000, backupCount=5)
 formatter = logging.Formatter("[%(asctime)s]  %(levelname)s in %(module)s: %(message)s")
 file_handler.setFormatter(formatter)
 
 console_handler = logging.StreamHandler()
 console_handler.setFormatter(formatter)
 
-if not logger.handlers():
+if not logger.hasHandlers():
     logger.addHandler(file_handler)
     logger.addHandler(console_handler)
